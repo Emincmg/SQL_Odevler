6ed8372a67480ed480a82d12c761e9841ce68f4e
Emincmg <106166521+Emincmg@users.noreply.github.com>
2022-10-24 13:42:00 +0300

    Update Odev1-3.Sorgu.sql

--- Odev1-3.Sorgu.sql
+++ Odev1-3.Sorgu.sql
@@ -1,1 +1,1 @@
-SELECT * FROM film WHERE rental_rate=0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
+SELECT * FROM film WHERE rental_rate=0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99 ORDER BY replacement_cost;
